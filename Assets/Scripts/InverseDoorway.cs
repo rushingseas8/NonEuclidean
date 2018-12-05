7b01f6e55ae5a726d17a6e49ca857b7f1cdca789
aleksandrovich <aleksandrovich@oath.com>
2018-12-05 17:41:22 -0600

    Cleaned up code slightly, added more minimal scene

--- Assets/Scripts/InverseDoorway.cs
+++ Assets/Scripts/InverseDoorway.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class InverseDoorway : MonoBehaviour {
 
+    /// <summary>
+    /// The main camera in the scene, that the player sees rendered to their screen.
+    /// The position of this relative to Source is used to render the texture.
+    /// </summary>
     [SerializeField]
-    private Controller player;
+    private Camera MainCamera;
 
+    /// <summary>
+    /// The source doorway. Where the texture gets rendered.
+    /// </summary>
     [SerializeField]
     private Transform Source;
 
+    /// <summary>
+    /// The target doorway. Where the source is connected to.
+    /// </summary>
     [SerializeField]
     private Transform Destination;
 
@@ -22,38 +32,19 @@ public class InverseDoorway : MonoBehaviour {
     {
         // We must enforce identical FOV and aspect ratio for the effect to work.
         cam = GetComponent<Camera>();
-        cam.aspect = player.mainCamera.aspect;
-        cam.fieldOfView = player.mainCamera.fieldOfView;
+        if (cam == null) {
+            cam = gameObject.AddComponent<Camera>();
+        }
+        cam.aspect = MainCamera.aspect;
+        cam.fieldOfView = MainCamera.fieldOfView;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //Vector3 playerPos = player.transform.position;
-        //Vector3 flippedPlayer = new Vector3(playerPos.x, playerPos.y, playerPos.z);
-        //Vector3 drawingPos = drawingDoorway.transform.position;
-        //Vector3 offset = playerPos;
-
-        //Vector3 flippedOffset = new Vector3(-offset.x, offset.y, -offset.z);
-
-        //transform.position = connectedDoorway.transform.position + flippedPlayer - drawingPos;
-        //transform.position = connectedDoorway.transform.position - drawingDoorway.transform.position + player.transform.position;
-        //transform.rotation = player.mainCamera.transform.rotation;
-
-
-        //Vector3 pos = connectedDoorway.transform.InverseTransformPoint(player.transform.position);
-        //Vector3 pos = connectedDoorway.transform.position - player.transform.position;
-        //transform.localPosition = new Vector3(-pos.x, pos.y, -pos.z);
-
-        //Vector3 euler = Vector3.zero;
-        //euler.y = SignedAngle(connectedDoorway.transform.forward, player.mainCamera.transform.forward, Vector3.up);
-
-        //transform.localRotation = Quaternion.Euler(euler);
-        //transform.localRotation = Quaternion.LookRotation(player.mainCamera.transform.forward - connectedDoorway.transform.forward, Vector3.up);
-
-        //Transform Source = drawingDoorway.transform;
-        //Transform Destination = connectedDoorway.transform;
-
-        Camera MainCamera = player.mainCamera;
+        // TODO: make the camera constantly check for changes in main camera FOV/aspect.
+        // TODO: change the resolution of renderTexture to always result in at least as much
+        // resolution as the player's screen. Based on distance(player, source) and size of
+        // player window.
 
         Vector3 cameraPositionInSourceSpace = Source.InverseTransformPoint(MainCamera.transform.position);
         Quaternion cameraRotationInSourceSpace = Quaternion.Inverse(Source.rotation) * MainCamera.transform.rotation;
@@ -61,22 +52,5 @@ public class InverseDoorway : MonoBehaviour {
         this.transform.position = Destination.TransformPoint(cameraPositionInSourceSpace);
         this.transform.rotation = Destination.rotation * cameraRotationInSourceSpace;
 
-    }
-
-    private float SignedAngle(Vector3 a, Vector3 b, Vector3 n)
-    {
-        //Code stolen from DiegoSLTS
-        //http://answers.unity3d.com/questions/992289/portal-effect-using-render-textures-how-should-i-m.html
-
-        // angle in [0,180]
-        float angle = Vector3.Angle(a, b);
-        float sign = Mathf.Sign(Vector3.Dot(n, Vector3.Cross(a, b)));
-
-        // angle in [-179,180]
-        float signed_angle = angle * sign;
-
-        while (signed_angle < 0) signed_angle += 360;
-
-        return signed_angle;
     }
 }
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A class defining a single Portal object. 
+/// </summary>
+public class Portal : MonoBehaviour {
+
+    /// <summary>
+    /// The main camera in the scene, that the player sees rendered to their screen.
+    /// The position of this relative to Source is used to render the texture.
+    /// </summary>
+    [SerializeField]
+    private Camera MainCamera;
+
+    /// <summary>
+    /// The Portal we're connected to.
+    /// </summary>
+    [SerializeField]
+    private Transform Destination;
+
+    private static readonly Quaternion _mirror = Quaternion.AngleAxis(180, Vector3.forward) * Quaternion.AngleAxis(180, Vector3.left);
+
+    /// <summary>
+    /// Should we normalize the transformation? If false, we use the attached
+    /// object's Transform as-is. If true, we pretend the attached object has
+    /// an identity Transform instead of a rotated one.
+    /// </summary>
+    [SerializeField]
+    private bool NormalizeTransform = false;
+
+    private GameObject childObject;
+
+    // A camera we create for the render texture.
+    private Camera renderCamera;
+
+    private RenderTexture renderTexture;
+    private Material renderMaterial;
+    private static Shader portalShader;
+
+	// Use this for initialization
+	void Start () {
+        // Get player camera (TODO: use the GameManager to assign this)
+        if (MainCamera == null) {
+            MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        }
+
+        // Find the portal shader, if it's not already set
+        if (portalShader == null)
+        {
+            portalShader = Shader.Find("Custom/Perspective");
+        }
+
+        childObject = new GameObject();
+        //childObject.transform.parent = this.transform;
+        childObject.name = "Camera looking at " + Destination.name;
+
+        // Set up our custom camera/renderTexture/material
+        renderCamera = childObject.AddComponent<Camera>();
+
+        // Don't render portals (or else things don't look great!)
+        renderCamera.cullingMask &= ~LayerMask.GetMask("Portal");
+
+        // TODO make this size adaptive based on screen size + player distance from this portal
+        // (can also completely stop rendering if player faces away from the object, i.e. the angle from
+        // MainCamera.transform.forward to (transform.position - player.position) > 180)
+        renderTexture = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGB32);
+        renderCamera.forceIntoRenderTexture = true;
+        renderCamera.targetTexture = renderTexture;
+
+        renderMaterial = new Material(portalShader);
+        renderMaterial.mainTexture = renderTexture;
+
+        // Assign this object's material to be the render material.
+        gameObject.GetComponent<Renderer>().material = renderMaterial;
+
+        // Update the render camera to always have same FOV + aspect ratio as the 
+        // player's camera. Otherwise we'll get weird discrepencies that are immediately
+        // visible.
+        UpdateRenderCamera();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        UpdateRenderCamera();
+
+        Vector3 cameraPositionInSourceSpace;
+        Quaternion cameraRotationInSourceSpace;
+
+        if (!NormalizeTransform)
+        {
+            //Vector3 mainCameraFlipped = new Vector3(-MainCamera.transform.position.x, MainCamera.transform.position.y, -MainCamera.transform.position.z);
+
+            cameraPositionInSourceSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+            cameraRotationInSourceSpace = Quaternion.Inverse(transform.rotation) * MainCamera.transform.rotation;
+        }
+        else
+        {
+            cameraPositionInSourceSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+            cameraRotationInSourceSpace = MainCamera.transform.rotation;
+        }
+
+        //cameraPositionInSourceSpace = new Vector3(cameraPositionInSourceSpace.x, -cameraPositionInSourceSpace.y, cameraPositionInSourceSpace.z);
+
+        // Pretend for a moment the portal is facing the opposite direction
+        Destination.Rotate(Vector3.forward, 180);
+
+        renderCamera.transform.position = Destination.TransformPoint(cameraPositionInSourceSpace);
+        renderCamera.transform.rotation = Destination.rotation * cameraRotationInSourceSpace;
+
+        // Make sure we don't actually keep it that way, though
+        Destination.Rotate(Vector3.forward, -180);
+
+    }
+
+    private bool done = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (done) return;
+        other.transform.position = Destination.position - new Vector3(0f, 4, 0);
+        done = true;
+    }
+
+    // Ensures that the settings of our custom rendering camera match the main 
+    // camera in all the right ways- aspect + FOV. Other settings might go here later.
+    private void UpdateRenderCamera()
+    {
+        renderCamera.aspect = MainCamera.aspect;
+        renderCamera.fieldOfView = MainCamera.fieldOfView;
+    }
+}
