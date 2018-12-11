b7f5cb8ee83214c56b799e1128089a89ea04a5b4
aleksandrovich <aleksandrovich@oath.com>
2018-12-11 16:47:00 -0600

    Added a test for multiscene loading.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+// Testing dynamic scene loading.
+public class GameManager : MonoBehaviour {
+
+    private static bool alreadyLoaded = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadScene());
+	}
+
+    private IEnumerator LoadScene() {
+
+        if (!alreadyLoaded)
+        {
+            AsyncOperation op = SceneManager.LoadSceneAsync("Tea Room", LoadSceneMode.Additive);
+            //op.allowSceneActivation = false;
+            alreadyLoaded = true;
+
+            yield return new WaitUntil(() => op.isDone);
+            Debug.Log("Done!");
+
+            op.completed += (a) =>
+            {
+                //op.allowSceneActivation = false   ;
+
+                Debug.Log("Loaded!");
+            };
+
+        }
+        yield return null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
